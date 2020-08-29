416c11ef51b7fabe44821198e15f5a08b73c8114
ometz123 <57960491+ometz123@users.noreply.github.com>
2020-08-29 22:04:30 +0300

    ui is done for now
    next is record

--- src/FC/FCPopcornTimer.jsx
+++ src/FC/FCPopcornTimer.jsx
@@ -1,23 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
+import { Input, CheckBox, ButtonGroup } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 export default function PopcornTimer(props) {
-    const [value, onChangeText] = useState('');
+    const [weight, setWeight] = useState('');
+    const buttons = ['Microwave ', 'Stovetop'];
+    const [instrument, setInstruments] = useState(0);
+
+    const listen = (weight, instrument) => {
+        alert(`weight: ${weight},instrument: ${instrument}`)
+    }
     return (
-        <View>
+        <ScrollView keyboardShouldPersistTaps='handled'>
             <Text style={props.styles.titleText}>Popcorn Timer{"\n"}</Text>
-            <TextInput
-                style={{ height: 40, borderColor: 'blue', borderWidth: 1 }}
-                onChangeText={text => onChangeText(text)}
-                value={value}
-                placeholder={"grams"}
-            />
+            <View>
+                <ButtonGroup
+                    onPress={(e) => setInstruments(e)}
+                    selectedIndex={instrument}
+                    buttons={buttons}
+                    containerStyle={{ height: 50 }}
+                    selectedButtonStyle={{ backgroundColor: '#6317fc' }}
+                />
+                <Input
+                    onChangeText={weight => setWeight(weight)}
+                    value={weight}
+                    keyboardType={'numeric'}
+                    label={'Weight (g)'}
+                    leftIcon={
+                        <Icon
+                            name='weight'
+                            size={24}
+                            color='black'
+                        />
+                    }
+                />
+            </View>
             <Text>
-                {"Hello\n"}
+                {"\n"}
             </Text>
-            <Button title="Start" onPress={() => { console.log("start"); }} />
-        </View>
+            <Button
+                color={"#6317fc"}
+                title="Start"
+                disabled={weight < 1}
+                onPress={() => listen(weight, instrument)}
+            />
+        </ScrollView >
     );
 }
 
