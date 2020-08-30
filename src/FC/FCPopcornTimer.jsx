0fe2063755fe98c5b515add2750754fdbb925dea
ometz123 <57960491+ometz123@users.noreply.github.com>
2020-08-30 20:34:52 +0300

    UI is fine
    Need Logic!

--- src/FC/FCPopcornTimer.jsx
+++ src/FC/FCPopcornTimer.jsx
@@ -1,51 +1,61 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button, ScrollView, ActivityIndicator } from 'react-native';
 import { Input, CheckBox, ButtonGroup } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 
 export default function PopcornTimer(props) {
+
     const [weight, setWeight] = useState('');
     const buttons = ['Microwave ', 'Stovetop'];
     const [instrument, setInstruments] = useState(0);
+    const [listening, setListening] = useState(false);
 
     const listen = (weight, instrument) => {
-        alert(`weight: ${weight},instrument: ${instrument}`)
+        console.log(`listen(${weight},${instrument})`);
+        setListening(true);
     }
+    const cancel = () => {
+        console.log("cancel()")
+        setListening(false);
+    }
+
+
     return (
         <ScrollView keyboardShouldPersistTaps='handled'>
-            <Text style={props.styles.titleText}>Popcorn Timer{"\n"}</Text>
             <View>
-                <ButtonGroup
-                    onPress={(e) => setInstruments(e)}
-                    selectedIndex={instrument}
-                    buttons={buttons}
-                    containerStyle={{ height: 50 }}
-                    selectedButtonStyle={{ backgroundColor: '#6317fc' }}
+                <Text style={props.styles.titleText}>Popcorn Timer{"\n"}</Text>
+                <View>
+                    <ButtonGroup
+                        onPress={(e) => setInstruments(e)}
+                        selectedIndex={instrument}
+                        buttons={buttons}
+                        containerStyle={{ height: 50 }}
+                        selectedButtonStyle={{ backgroundColor: '#6317fc' }}
+                    />
+                    <Input
+                        onChangeText={weight => setWeight(weight)}
+                        value={weight}
+                        keyboardType={'numeric'}
+                        label={'Weight (g)'}
+                        leftIcon={
+                            <Icon
+                                name='weight'
+                                size={24}
+                                color='black'
+                            />
+                        }
+                    />
+                </View>
+                <Button
+                    color={listening ? 'red' : "#6317fc"}
+                    title={listening ? "Cancel" : "Start"}
+                    disabled={weight < 1}
+                    onPress={() => listening ? cancel() : listen(weight, instrument)}
                 />
-                <Input
-                    onChangeText={weight => setWeight(weight)}
-                    value={weight}
-                    keyboardType={'numeric'}
-                    label={'Weight (g)'}
-                    leftIcon={
-                        <Icon
-                            name='weight'
-                            size={24}
-                            color='black'
-                        />
-                    }
-                />
+                <View style={{ margin: 30 }}>
+                    {listening && <ActivityIndicator size="large" color="#1767fc" />}
+                </View>
             </View>
-            <Text>
-                {"\n"}
-            </Text>
-            <Button
-                color={"#6317fc"}
-                title="Start"
-                disabled={weight < 1}
-                onPress={() => listen(weight, instrument)}
-            />
         </ScrollView >
     );
 }
-
