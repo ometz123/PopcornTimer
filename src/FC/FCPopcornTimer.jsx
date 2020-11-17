b4b53b2d1df2d02e6cd3f4a9e1b46ebe06419dcd
ometz123 <57960491+ometz123@users.noreply.github.com>
2020-11-17 12:55:58 +0200

    Update

--- src/FC/FCPopcornTimer.jsx
+++ src/FC/FCPopcornTimer.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Button, ScrollView, ActivityIndicator } from 'react-native';
+import { StyleSheet, Text, View, Button, ScrollView, ActivityIndicator, TouchableOpacity } from 'react-native';
 import { Input, CheckBox, ButtonGroup } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 
@@ -11,14 +11,25 @@ export default function PopcornTimer(props) {
     const [listening, setListening] = useState(false);
 
     const listen = (weight, instrument) => {
-        console.log(`listen(${weight},${instrument})`);
-        setListening(true);
-    }
-    const cancel = () => {
-        console.log("cancel()")
-        setListening(false);
+        if (!listening) {
+            console.log(`listen(${weight},${instrument})`);
+            setListening(true);
+        } else {
+            console.log("cancel()");
+            setListening(false);
+        }
     }
 
+    useEffect(() => {
+        navigator.mediaDevices.getUserMedia({ audio: true })
+            .then(mediaStream => {
+                //mediaStream.
+            })
+            .catch(error => console.error(error));
+    }, []);
+    const playSound = () => {
+        console.log('meaw');
+    }
 
     return (
         <ScrollView keyboardShouldPersistTaps='handled'>
@@ -50,11 +61,16 @@ export default function PopcornTimer(props) {
                     color={listening ? 'red' : "#6317fc"}
                     title={listening ? "Cancel" : "Start"}
                     disabled={weight < 1}
-                    onPress={() => listening ? cancel() : listen(weight, instrument)}
+                    onPress={() => listen(weight, instrument)}
                 />
                 <View style={{ margin: 30 }}>
                     {listening && <ActivityIndicator size="large" color="#1767fc" />}
                 </View>
+                <Button
+                    title="Play"
+                    onPress={() => playSound()}
+                />
+
             </View>
         </ScrollView >
     );
